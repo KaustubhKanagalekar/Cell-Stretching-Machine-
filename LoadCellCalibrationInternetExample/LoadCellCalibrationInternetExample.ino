6e84f341180f4bf9b413195b01c03ebbb0063fd0
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2025-05-14 20:48:08 -0700

    load cell readings code (w time sync issue)

--- LoadCellCalibrationInternetExample/LoadCellCalibrationInternetExample.ino
+++ LoadCellCalibrationInternetExample/LoadCellCalibrationInternetExample.ino
@@ -42,30 +42,30 @@ float calibration_factor = -220000; //-7050 worked for my 440lb max scale setup
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 calibration sketch");
+  /*Serial.println("HX711 calibration sketch");
   Serial.println("Remove all weight from scale");
   Serial.println("After readings begin, place known weight on scale");
   Serial.println("Press + or a to increase calibration factor");
   Serial.println("Press - or z to decrease calibration factor");
-
+*/
   scale.begin(DOUT, CLK);
   scale.set_scale();
   scale.tare(); //Reset the scale to 0
 
   long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
+  //Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  //Serial.println(zero_factor);
 }
 
 void loop() {
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
-  Serial.println("Reading: ");
+  //Serial.print("Reading: ");
   Serial.print(scale.get_units(), 1);
-  Serial.println(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
-  Serial.print("calibration_factor: ");
-  Serial.print(calibration_factor);
+  //Serial.println(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  //Serial.print("calibration_factor: ");
+  //Serial.print(calibration_factor);
   Serial.println();
 
   if(Serial.available())
